389fc3bbdd6deb351e367a3c9ee36dd8b2611bf3
Danil Redko <danilred2002@gmail.com>
2025-08-23 11:09:21 +0500

    pre-photo

--- src/data/massageData.js
+++ src/data/massageData.js
@@ -6,13 +6,13 @@ export const massageContent = {
   hero: {
     title: "МАССАЖ В",
     subtitle: "Массаж — это бережное лечение через прикосновение.",
-    image: "/images/massage/hero.jpg",
+    image: "/images/heromassage.jpg",
     cta: "Записаться на массаж",
   },
   intro: {
     title: "Массаж в студиях",
     text: "Помогаем снять мышечное напряжение, улучшить осанку и качество сна. Работаем по записи, стерильно и бережно. Первый визит — со скидкой.",
-    image: "/images/massage/intro.jpg",
+    image: "/images/massage4.jpg",
     typesTitle: "Виды массажа, представленные в студиях:",
     types: [
       "Классический",
@@ -27,17 +27,17 @@ export const massageContent = {
   benefitsTitle: "Зачем посещать массаж в студиях",
   benefits: [
     {
-      image: "/images/massage/benefit-1.jpg",
+      image: "/images/massage2.jpg",
       title: "Снятие стресса и тревожности",
       note: "30–60 минут глубокого расслабления",
     },
     {
-      image: "/images/massage/benefit-2.jpg",
+      image: "/images/massage3.jpg",
       title: "Здоровая спина и осанка",
       note: "Регулярная работа с зажимами",
     },
     {
-      image: "/images/massage/benefit-3.jpg",
+      image: "/images/massage5.jpg",
       title: "Быстрее восстановление",
       note: "После тренировок и нагрузок",
     },
@@ -61,21 +61,21 @@ export const massageContent = {
       name: "Юнусабадский район, Ц2",
       address: "проезд Олой Бозори 59/3",
       phone: "+998 93 377 56 97",
-      photo: "/images/massage/studio-1.jpg",
-      mapLink: "https://maps.google.com",
+      photo: "/images/st-1.jpg",
+      mapLink: "https://www.google.com/maps/place/Reforma+fitness/@41.3204968,69.2823763,17z/data=!4m14!1m7!3m6!1s0x38aef570b0ae0081:0x33f9142c2a58e58!2sReforma+fitness!8m2!3d41.3204969!4d69.2872472!16s%2Fg%2F11g0hm1cs0!3m5!1s0x38aef570b0ae0081:0x33f9142c2a58e58!8m2!3d41.3204969!4d69.2872472!16s%2Fg%2F11g0hm1cs0?entry=ttu&g_ep=EgoyMDI1MDgxOS4wIKXMDSoASAFQAw%3D%3D",
     },
     {
       name: "Яшнабадский район",
       address: "улица Алимкент 33",
       phone: "+998 93 377 56 97",
-      photo: "/images/massage/studio-2.jpg",
-      mapLink: "https://maps.google.com",
+      photo: "/images/st-2.jpg",
+      mapLink: "https://www.google.com/maps/place/Alimkent+ko'chasi+33,+Тоshkent,+Toshkent,+Узбекистан/@41.2849495,69.3275546,15.46z/data=!4m5!3m4!1s0x38aef578f0b4568d:0xf30cbce1f1407c1c!8m2!3d41.2848108!4d69.333513?entry=ttu&g_ep=EgoyMDI1MDgxOS4wIKXMDSoASAFQAw%3D%3D",
     },
   ],
   trial: {
     title: "Запишись на пробный сеанс массажа",
     text: "На первом сеансе специалист оценит состояние мышц и подскажет, какой формат вам подойдёт лучше. После — рекомендации по уходу и растяжке.",
-    image: "/images/massage/trial.jpg",
+    image: "/images/heromassage.jpg",
     cta: "Записаться на пробный сеанс",
   },
 };
